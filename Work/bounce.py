142cce5346df415f29ce1c99c5909a9f0df7d590
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2021-10-05 23:12:27 -0400

    Complete exercises through 1.8
    Making this commit so that I can keep track of how much work I have done

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+ORIGINAL_DROP = 100
+bounce = ORIGINAL_DROP
+bounce_hight = 3/5
+
+for bounces in range(10):
+    bounce = bounce * bounce_hight
+    bounce = round(bounce, 4)
+    print(bounces + 1, bounce)
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,20 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+months = 0
+principle = 500000
+rate = .05
+payment = 2684.11
+total_paid = 0.0
+
+while principle > 0:
+    months += 1
+    if months <= 12:
+        principle = principle * (1+rate/12) - (payment + 1000)
+        total_paid = total_paid + (payment + 1000)
+    else:
+        principle = principle * (1+rate/12) - payment
+        total_paid = total_paid + payment
+
+print('Total paid', total_paid, 'Over', months, 'months')
