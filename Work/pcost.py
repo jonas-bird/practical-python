2e53702144a9510a4eb4b0d8666fb5d2a1194622
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2021-12-15 13:27:48 -0500

    modify pcost.py for exercise 1.30
    put opening the file and summing the product of each row's second and
    third column into a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,12 +3,19 @@
 #
 # Exercise 1.27
 
-total_price = 0.0
 
-with open('Data/portfolio.csv', 'rt') as f:
-    header = next(f)
-    for line in f:
-        row = line.split(',')
-        total_price += int(row[1]) * float(row[2])
+def portfolio_cost(filename):
+    """ takes the filename of a csv file with an integer in the second column
+and a double in the third, and returns the sum of the product of the two
+columns in each row"""
+    total_price = 0.0
+    with open(filename, 'rt') as f:
+        header = next(f)
+        for line in f:
+            row = line.split(',')
+            total_price += int(row[1]) * float(row[2])
+    return total_price
 
-print('Total cost ', total_price)
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost: ', cost)
