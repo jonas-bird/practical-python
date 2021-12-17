92339288a99785f6057c9f90be337d90635ef7b1
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2021-12-16 20:48:33 -0500

    modify report.py for exercise 2.9,
    fix return statement of read_prices do it runs.
    add make_report function

--- Work/report.py
+++ Work/report.py
@@ -12,6 +12,7 @@ def read_portfolio(filename):
 
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
+        # should be name, shares, price
         headers = next(rows)
         for row in rows:
             holding = {}
@@ -37,7 +38,23 @@ def read_prices(filename):
             except:
                 continue
 
-    return prices
+    return stock_prices
+
+
+def make_report(stock_list, prices_dict):
+    """takes a list of dictionaries representing stock holdings and a
+    dictionary of stock symbols and the current price and returns a list
+    of tuples"""
+    stock_status = []
+    for stock in stock_list:
+        stock_info = {
+            'name': stock['name'],
+            'shares': stock['shares'],
+            'price': prices_dict[stock['name']],
+            'change': (prices_dict[stock['name']] - stock['price'])
+        }
+        stock_status.append(stock_info)
+    return stock_status
 
 
 if __name__ == '__main__':
@@ -48,15 +65,19 @@ if __name__ == '__main__':
     else:
         holdings_file = 'Data/portfolio.csv'
         prices_file = 'Data/prices.csv'
-    stocks_change = {}
-    total_value = 0.0
-    old_value = 0.0
+
     stocks = read_portfolio(holdings_file)
     prices = read_prices(prices_file)
+    stock_report = make_report(stocks, prices)
 
-    for stock in stocks:
-        total_value += (stock['shares'] * prices[stock['name']])
-        old_value += (stock['shares'] * stock['price'])
+    #  stocks_change = {}
+    # total_value = 0.0
+    # old_value = 0.0
 
-    print("Your portfolio is worth ${}, it has changed by ${}".format(
-        total_value, (total_value - old_value))
+#    for stock in stocks:
+
+       #  total_value += (stock['shares'] * prices[stock['name']])
+    #     old_value += (stock['shares'] * stock['price'])
+
+    # print("Your portfolio is worth ${}, it has changed by ${}".format(
+    #     total_value, (total_value - old_value))
