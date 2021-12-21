50fc3c52ad18d50ee0ed60781552236ddb9427c3
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2021-12-21 00:35:20 -0500

    Modify fileparse.py for exercise 3.6
    Change parse_csv to handle CSV files without headers

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,36 +5,51 @@
 import csv
 
 
-def parse_csv(filename, select=None, types=None):
+def parse_csv(filename, select=None, types=None, has_headers=False):
     """
     Parse a CSV file into a list of records
     """
     with open(filename) as f:
         rows = csv.reader(f)
+        if has_headers:
+            # Read the file headers
+            headers = next(rows)
 
-        # Read the file headers
-        headers = next(rows)
-
-        # If a column selector was given, find indices of the specified columns.
-        # Also narrow the set of headers used for resulting dictionaries
-        # TODO fix this so it handles being given colnames that are not in the CSV
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
+            # If a column selector was given, find indices of the specified columns.
+            # Also narrow the set of headers used for resulting dictionaries
+            # TODO fix this so it handles being given colnames that are not in the CSV
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+            else:
+                indices = []
 
         records = []
-        for row in rows:
+        for rowno, row in enumerate(rows, start=1):
             if not row:     # Skip rows with no data
                 continue
-            # Filter the row if specific columns were selected
-            if indices:
-                row = [ row[index] for index in indices ]
+            # if type data for the rows was specified in the function call
+            # typcast the row accordingly
             if types:
-                row = [func(val) for func, val in zip(types, row)]
-            # Make a dictionary
-            record = dict(zip(headers, row))
+                # I am not sure if we are supposed to do this error check but...
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except ValueError:
+                     print(f'Row {rowno}: Bad row: {row}')
+                     continue
+
+            # if the CSV file has headers create a dictionary using them
+            if has_headers:
+                # Filter the row if specific columns were selected
+                if indices:
+                    row = [ row[index] for index in indices ]
+
+                record = dict(zip(headers, row))
+            # if the CSV file does not have headers create a tuple from each row
+            else:
+                record = tuple(row)
+
+            # add the row to a list
             records.append(record)
 
     return records
