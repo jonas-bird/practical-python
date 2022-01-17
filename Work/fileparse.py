d8c02b66d71f7297d6a0916ae86bfb6e2f12fd7b
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-01-17 03:16:23 -0500

    Modify fileparse.py for exercise 3.8 and 3.9

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -9,6 +9,9 @@ def parse_csv(filename, select=None, types=None, has_headers=False, delimiter=',
     """
     Parse a CSV file into a list of records
     """
+    if select is not None and has_headers is False:
+        raise RuntimeError('select argument requires column headers')
+
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
         if has_headers:
@@ -17,7 +20,6 @@ def parse_csv(filename, select=None, types=None, has_headers=False, delimiter=',
 
             # If a column selector was given, find indices of the specified columns.
             # Also narrow the set of headers used for resulting dictionaries
-            # TODO fix this so it handles being given colnames that are not in the CSV
             if select:
                 indices = [headers.index(colname) for colname in select]
                 headers = select
@@ -34,9 +36,10 @@ def parse_csv(filename, select=None, types=None, has_headers=False, delimiter=',
                 # I am not sure if we are supposed to do this error check but...
                 try:
                     row = [func(val) for func, val in zip(types, row)]
-                except ValueError:
-                     print(f'Row {rowno}: Bad row: {row}')
-                     continue
+                except ValueError as e:
+                    print(f'Row {rowno}: Bad row: {row}')
+                    print(e)
+                    continue
 
             # if the CSV file has headers create a dictionary using them
             if has_headers:
