c1a91931b56ea29fa99452ea7fa3ffad63f6c2d7
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-06-21 12:34:19 -0400

    feat: modify follow to display stocks in portfolio
    complete exercise 6.7

--- Work/follow.py
+++ Work/follow.py
@@ -5,6 +5,7 @@ https://dabeaz-course.github.io/practical-python/
 """
 import os
 import time
+import report
 
 
 def follow(aFile):
@@ -20,10 +21,12 @@ def follow(aFile):
 
 
 if __name__ == '__main__':
+
+    portfolio = report.read_portfolio('Data/portfolio.csv')
     for line in follow('Data/stocklog.csv'):
         fields = line.split(',')
         name = fields[0].strip('""')
         price = float(fields[1])
         change = float(fields[4])
-        if change < 0:
+        if name in portfolio:
             print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
