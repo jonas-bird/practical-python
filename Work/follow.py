247aec521f59065b743fbda2e8d03da1a72132d3
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-06-21 12:23:17 -0400

    refactor: Modify follow.py to use generator
    Move code to produce lines from csv file into generator function
    Complete exercise 6.6

--- Work/follow.py
+++ Work/follow.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python3 
 """
-Code from practical python  https://dabeaz-course.github.io/practical-python/Notes/06_Generators/02_Customizing_iteration.html
+Code from practical python course
+https://dabeaz-course.github.io/practical-python/
 """
 import os
-import time 
+import time
 
-f = open('Data/stocklog.csv')
-f.seek(0, os.SEEK_END)  # Move file pointer 0 bytes from end of file
-while True:
-	line = f.readline()
-	if line == '':
-		time.sleep(0.1)  # pause before retry
-		continue
-	fields = line.split(',')
-	name = fields[0].strip('"')
-	price = float(fields[1])
-	change = float(fields[4])
-	if change < 0:
-		print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+
+def follow(aFile):
+    # generator that produces lines from a file
+    f = open(aFile)
+    f.seek(0, os.SEEK_END)  # Move file handle to end of file
+    while True:
+        newLine = f.readline()
+        if newLine == '':
+            time.sleep(0.1)
+            continue
+        yield newLine
+
+
+if __name__ == '__main__':
+    for line in follow('Data/stocklog.csv'):
+        fields = line.split(',')
+        name = fields[0].strip('""')
+        price = float(fields[1])
+        change = float(fields[4])
+        if change < 0:
+            print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
