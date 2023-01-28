a58fd16c52f7ad51f27bf86aa9983f477f47b34b
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2023-01-27 19:18:09 -0500

    feat: complete exercise 6.10
    Add functions to ticker.py to process a stream of rows in CSV format
    through generator functions that convert the data type of the entries in
    each row (based on providing a data type for each column) and builds
    dictionaries from the the selected and converted rows and a list of keys.

--- Work/ticker.py
+++ Work/ticker.py
@@ -13,10 +13,19 @@ def select_columns(rows, indices):
     for row in rows:
         yield [row[index] for index in indices]
 
+def convert_types(rows, types):
+    for col in rows:
+        yield [func(val) for func, val in zip(types, col)]
+
+def make_dicts(rows, headers):
+    for row in rows:
+        yield dict(zip(headers, row))
 
 def parse_stock_data(lines_in):
     csv_rows = csv.reader(lines_in)
     csv_rows = select_columns(csv_rows, [0, 1, 4])
+    csv_rows = convert_types(csv_rows, [str, float, float])
+    csv_rows = make_dicts(csv_rows, ['name', 'price', 'change'])
     return csv_rows
 
 
